403dca0c13d185003e7669514abe0a2ac168d5c0
Moisés Gomes <mosesgdo@gmail.com>
2023-12-26 10:22:10 -0300

    modificado clienteAplicationService e renomeado clienteRepository

---
+++ src/main/java/br/com/petz/clientepet/cliente/application/domain/cliente/ClienteRepository.java
@@ -0,0 +1,5 @@
+package br.com.petz.clientepet.cliente.application.domain.cliente;
+
+public class ClienteRepository {
+
+}
--- src/main/java/br/com/petz/clientepet/cliente/application/service/ClienteApplicationService.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/service/ClienteApplicationService.java
@@ -5,7 +5,8 @@ import org.springframework.stereotype.Service;
 import br.com.petz.clientepet.cliente.application.api.ClienteRequest;
 import br.com.petz.clientepet.cliente.application.api.ClienteResponse;
 import br.com.petz.clientepet.cliente.application.api.ClienteResponse.ClienteResponseBuilder;
-import br.com.petz.clientepet.cliente.application.repository.ClienteRepository;
+import br.com.petz.clientepet.cliente.application.domain.cliente.ClienteRepository;
+import br.com.petz.clientepet.cliente.application.repository.clienteRepsitory;
 import br.com.petz.clientepet.cliente.domain.Cliente;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
@@ -15,10 +16,11 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 @RequiredArgsConstructor
 public class ClienteApplicationService implements ClienteService {
+	private final ClienteRepository clienteRepository = new ClienteRepository();
   @Override
 	public ClienteResponse criaCliente(ClienteRequest clienteRequest) {
 		log.info("[inicia ]ClienteApplicationService - criaCliente");
-		Cliente cliente = new Cliente(null);
+		Cliente cliente = clienteRepsitory.salva (new Cliente(clienteRequest));
 		log.info("[finaliza ]ClienteApplicationService - criaCliente");
 		return ClienteResponse.builder()
 				.idCliente(cliente.getIdCliente())
