992a36f54a7cdc021b5d63af2ff2a58b0e43c37c
Moisés Gomes <mosesgdo@gmail.com>
2024-01-17 16:47:12 -0300

    implementa alteracao pet camada controller e aplicação

--- src/main/java/br/com/petz/clientepet/pet/application/service/PetService.java
+++ src/main/java/br/com/petz/clientepet/pet/application/service/PetService.java
@@ -3,6 +3,7 @@ package br.com.petz.clientepet.pet.application.service;
 import java.util.List;
 import java.util.UUID;
 
+import br.com.petz.clientepet.pet.application.api.PetAlteracaoRequest;
 import br.com.petz.clientepet.pet.application.api.PetClienteDetalhadoResponse;
 import br.com.petz.clientepet.pet.application.api.PetClienteListResponse;
 import br.com.petz.clientepet.pet.application.api.PetRequest;
@@ -14,5 +15,6 @@ public interface PetService {
 	List<PetClienteListResponse> buscaPetsDoClienteComID(UUID idCliente);
 	PetClienteDetalhadoResponse buscaPetDoClienteComID(UUID idCliente, UUID idPet);
 	void deletePetDoClienteComID(UUID idCliente, UUID idPet);
+	void alteraPetDoClienteComID(UUID idCliente, UUID idPet, PetAlteracaoRequest petAlteracaoRequest);
 
 }
