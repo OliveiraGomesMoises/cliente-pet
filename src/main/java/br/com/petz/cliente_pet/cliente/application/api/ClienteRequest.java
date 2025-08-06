feecf9ff9e2954f110a4fcf4223a6a467cb3fbd7
OliveiraGomesMoises <mosesgdo@gmail.com>
2025-08-06 16:32:53 -0300

    implementa ClienteRequest e ClineteResponse

--- src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteRequest.java
+++ src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteRequest.java
@@ -1,4 +1,32 @@
 package br.com.petz.cliente_pet.cliente.application.api;
 
+import br.com.petz.cliente_pet.cliente.domain.Sexo;
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Value;
+import org.hibernate.validator.constraints.br.CPF;
+
+import java.time.LocalDate;
+
+@Value
 public class ClienteRequest {
+    @NotBlank
+    public String nomeCompleto;
+    @NotBlank
+    @Email
+    @Column(unique = true)
+    public String email;
+    @NotBlank
+    public String celular;
+    public String telefone;
+    public Sexo sexo;
+    @NotNull
+    public LocalDate dataNascimento;
+    @CPF
+    @Column(unique = true)
+    public String cpf;
+    @NotNull
+    public Boolean aceitaTermos;
 }
--- src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteResponse.java
+++ src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteResponse.java
@@ -1,4 +1,12 @@
 package br.com.petz.cliente_pet.cliente.application.api;
 
+import lombok.Builder;
+import lombok.Value;
+
+import java.util.UUID;
+
+@Value
+@Builder
 public class ClienteResponse {
+    public UUID idCliente;
 }
