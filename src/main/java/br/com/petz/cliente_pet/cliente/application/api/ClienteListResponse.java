3db926e08c1db88b539b1af1d8fe2f1df1a8f991
OliveiraGomesMoises <mosesgdo@gmail.com>
2025-08-07 12:47:22 -0300

    implementa getTodos

--- src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteListResponse.java
+++ src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteListResponse.java
@@ -1,5 +1,6 @@
 package br.com.petz.cliente_pet.cliente.application.api;
 
+import br.com.petz.cliente_pet.cliente.domain.Cliente;
 import br.com.petz.cliente_pet.cliente.domain.Sexo;
 import jakarta.persistence.Column;
 import jakarta.validation.constraints.Email;
@@ -9,7 +10,9 @@ import lombok.Value;
 import org.hibernate.validator.constraints.br.CPF;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Value
 public class ClienteListResponse {
@@ -18,4 +21,17 @@ public class ClienteListResponse {
     public String email;
     public String celular;
     public String cpf;
+
+    public static List<ClienteListResponse> converte(List<Cliente> clientes) {
+        return clientes.stream()
+                .map(ClienteListResponse::new)
+                .collect(Collectors.toList());
+    }
+    public ClienteListResponse(Cliente cliente) {
+        this.idCliente = cliente.getIdCliente();
+        this.nomeCompleto = cliente.getNomeCompleto();
+        this.email = cliente.getEmail();
+        this.celular = cliente.getCelular();
+        this.cpf = cliente.getCpf();
+    }
 }
