31ff0abad5532507d549f95033bc615d7f9435ed
OliveiraGomesMoises <mosesgdo@gmail.com>
2025-08-08 10:40:51 -0300

    removi alguns imports não utilizados.

--- src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteAPI.java
+++ src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteAPI.java
@@ -1,7 +1,6 @@
 package br.com.petz.cliente_pet.cliente.application.api;
 
 import jakarta.validation.Valid;
-import lombok.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
